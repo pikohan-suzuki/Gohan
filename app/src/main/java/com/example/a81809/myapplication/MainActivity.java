5c65eb11b7543482b3b3ca4ab252bfeb6682c574
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2018-10-28 12:40:02 +0900

    GPSパーミッション確認用の処理を追加

--- app/src/main/java/com/example/a81809/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a81809/myapplication/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.a81809.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //パーミッションの確認
+        final int requestCode=1000;
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //許可されていない場合
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,},requestCode);
+        }
     }
 }
